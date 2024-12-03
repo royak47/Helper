6b5763824a1a10f3094cda2402e619643ffcad78
royak47 <52905147+royak47@users.noreply.github.com>
2024-12-03 16:08:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,6 @@
-TELEGRAM_BOT_TOKEN=7374435705:AAGE7aT1dj1oZpbQh1bojwITIFMdYSQko8k
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Load environment variables from .env file
+
+TOKEN = os.getenv("7374435705:AAGE7aT1dj1oZpbQh1bojwITIFMdYSQko8k")
