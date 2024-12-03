7648fdbaefbf6fac15a42976a9dfd5cce26476c1
royak47 <52905147+royak47@users.noreply.github.com>
2024-12-03 15:11:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,31 +1,30 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackQueryHandler, Filters
-from commands import start, help, admin, filters
+from telegram.ext import Application, CommandHandler, MessageHandler, CallbackQueryHandler, filters
+from commands import start, help, admin, filters as filter_commands
 from config import TOKEN
 
 def main():
-    updater = Updater(TOKEN)
-    dispatcher = updater.dispatcher
+    application = Application.builder().token(TOKEN).build()
 
     # Command Handlers
-    dispatcher.add_handler(CommandHandler("start", start.start_command))
-    dispatcher.add_handler(CommandHandler("help", help.help_command))
-    dispatcher.add_handler(CommandHandler("ban", admin.ban_user))
-    dispatcher.add_handler(CommandHandler("mute", admin.mute_user))
-    dispatcher.add_handler(CommandHandler("unban", admin.unban_user))
-    dispatcher.add_handler(CommandHandler("unmute", admin.unmute_user))
-    dispatcher.add_handler(CommandHandler("filter", filters.add_filter))
-    dispatcher.add_handler(CommandHandler("deletefilter", filters.delete_filter))
-    dispatcher.add_handler(CommandHandler("listfilters", filters.list_filters))
+    application.add_handler(CommandHandler("start", start.start_command))
+    application.add_handler(CommandHandler("help", help.help_command))
+    application.add_handler(CommandHandler("ban", admin.ban_user))
+    application.add_handler(CommandHandler("mute", admin.mute_user))
+    application.add_handler(CommandHandler("unban", admin.unban_user))
+    application.add_handler(CommandHandler("unmute", admin.unmute_user))
+    application.add_handler(CommandHandler("filter", filter_commands.add_filter))
+    application.add_handler(CommandHandler("deletefilter", filter_commands.delete_filter))
+    application.add_handler(CommandHandler("listfilters", filter_commands.list_filters))
 
     # Filters handler
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, filters.handle_filters))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, filter_commands.handle_filters))
 
     # Callback Query Handler for inline buttons
-    dispatcher.add_handler(CallbackQueryHandler(help.help_callback))
+    application.add_handler(CallbackQueryHandler(help.help_callback))
 
     # Start the bot
-    updater.start_polling()
-    updater.idle()
+    print("Bot is running...")
+    application.run_polling()
 
 if __name__ == "__main__":
     main()
