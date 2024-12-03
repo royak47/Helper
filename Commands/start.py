c75a16bfd2f72f0addcb8eb3dca8321a6c272281
royak47 <52905147+royak47@users.noreply.github.com>
2024-12-03 02:22:36 -0800

    Delete Commands directory

--- Commands/start.py
+++
@@ -1,6 +0,0 @@
-from telegram import Update
-from telegram.ext import CallbackContext
-
-def start_command(update: Update, context: CallbackContext):
-    update.message.reply_text("Hello! Welcome to the bot. Use /help to see what I can do.")
-    update.message.delete()
