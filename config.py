f452710c483c057bf9210a21e7d09b0f563232a2
royak47 <52905147+royak47@users.noreply.github.com>
2024-12-03 16:18:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,10 @@ import os
 
 load_dotenv()  # Load environment variables from .env file
 
-TOKEN = os.getenv("7374435705:AAGE7aT1dj1oZpbQh1bojwITIFMdYSQko8k")
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+
+if not TOKEN:
+    print("Error: No token found. Please set the TELEGRAM_BOT_TOKEN environment variable.")
+    exit()  # Exit if no token found
+else:
+    print(f"Bot token is loaded: {TOKEN[:10]}...")  # Print part of the token for verification
